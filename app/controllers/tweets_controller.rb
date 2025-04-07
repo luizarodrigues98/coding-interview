f9729b9b21384b35862f098de7cd00fd3cd8072a
Luiza Lopes <t.luiza.lopes@fabrica.locaweb.com.br>
2025-04-07 13:44:09 -0300

    Implement pagination for tweets in index action, returning current page, total pages, and total count in JSON response.

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,5 +1,20 @@
 class TweetsController < ApplicationController
   def index
-    render json: Tweet.all
+    page = (params[:page] || 1).to_i
+    per_page = (params[:per_page] || 10).to_i
+    
+    tweets = Tweet.order(created_at: :desc)
+                  .limit(per_page)
+                  .offset((page - 1) * per_page)
+    total_count = Tweet.count
+    
+    render json: {
+      tweets: tweets,
+      pagination: {
+        current_page: page,
+        total_pages: (total_count.to_f / per_page).ceil,
+        total_count: total_count
+      }
+    }
   end
 end
